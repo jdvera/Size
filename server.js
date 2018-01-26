64083c075b7df586fa5956d04b134ab6b01fbf96
Desiree Tien <desireetien@Aphesis.local>
2018-01-26 10:41:46 -0600

    Updating layout basics

--- server.js
+++ server.js
@@ -1,29 +1,94 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
+var express = require('express');
+var app = express();
 
-// Configure body parser for AJAX requests
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// Serve up static assets
-app.use(express.static("client/build"));
-// Add routes, both API and view
-app.use(routes);
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-  {
-    useMongoClient: true
+/************************************************************
+ *
+ * Express routes for:
+ *   - app.js
+ *   - style.css
+ *   - index.html
+ *
+ *   Sample endpoints to demo async data fetching:
+ *     - POST /landing
+ *     - POST /home
+ *
+ ************************************************************/
+
+// Serve application file depending on environment
+app.get('/app.js', function(req, res) {
+  if (process.env.PRODUCTION) {
+    res.sendFile(__dirname + '/build/app.js');
+  } else {
+    res.redirect('//localhost:9090/build/app.js');
   }
-);
+});
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Serve aggregate stylesheet depending on environment
+app.get('/style.css', function(req, res) {
+  if (process.env.PRODUCTION) {
+    res.sendFile(__dirname + '/build/style.css');
+  } else {
+    res.redirect('//localhost:9090/build/style.css');
+  }
+});
+
+app.use(express.static(__dirname + '/build'));
+
+// Serve index page
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/build/index.html');
+});
+
+app.post('/landing', function(req, res) {
+  res.json({
+    title: "Landing Page"
+  });
+});
+
+app.post('/home', function(req, res) {
+  res.json({
+    title: "Home Page"
+  });
+});
+
+
+/*************************************************************
+ *
+ * Webpack Dev Server
+ *
+ * See: http://webpack.github.io/docs/webpack-dev-server.html
+ *
+ *************************************************************/
+
+if (!process.env.PRODUCTION) {
+  var webpack = require('webpack');
+  var WebpackDevServer = require('webpack-dev-server');
+  var config = require('./webpack.local.config');
+
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    noInfo: true,
+    historyApiFallback: true
+  }).listen(9090, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+
+/******************
+ *
+ * Express server
+ *
+ *****************/
+
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Essential React listening at http://%s:%s', host, port);
 });

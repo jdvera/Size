0e9d634d2cc41f45014ecc434d54a6571a9646a7
Desiree Tien <desireetien@Aphesis.local>
2018-01-29 14:33:53 -0600

    Pushing broken main page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -21,8 +21,41 @@ render() {
         </div>
        
          <div className="search">
-         <form>
+        <form className="form">
          <h1 className="h1">Size</h1>
+
+         <label for="clothingtype">Clothing Type</label>
+        <select id="clothingtype" name="clothingtype">
+        <option value="shoes">Shoes</option>
+        </select>
+
+        <label for="brand">Brand</label>
+        <select id="brand" name="brand">
+        
+       
+        
+       
+        <option value="vans">Vans</option>
+        <option value="stevemadden">Steve Madden</option>
+        <option value="underarmor">Under Armor</option>
+        
+        <option value="dcshoes">DCShoes</option>
+        <option value="reebok">Reebok</option>
+        
+        <option value="bcbg">BCBG</option>
+        <option value="puma">Puma</option>
+
+        <option value="adidas">Adidas</option>
+        <option value="asics">Asics</option>
+        <option value="converse">Converse</option>
+        <option value="newbalance">NewBalance</option>
+        <option value="nike">Nike</option>
+        <option value="ninewest">NineWest</option>
+
+        </select>
+
+
+        
   <input className="input" type="text" name="search" placeholder="What are you looking for?"/>
   <input className="input"type="submit" value="Submit"/>
 </form>

1962218c2afc4d4184bb1d90e0cc1f1f7780769b
Carlos <carlosfgutier@gmail.com>
2018-01-30 13:52:59 -0600

    ready to map results

--- client/src/components/Sizes/Sizes.js
+++ client/src/components/Sizes/Sizes.js
@@ -1,35 +1,31 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 import "./Sizes.css";
 
-const Sizes = () =>
-	<ul>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-		<li>Adidas</li>
-	</ul>
+class Sizes extends Component {
 
+	//make get request
+	//set state to item
+
+	//render
+	//add this code inside ul. It will repeat for every item in database
+	//this.state.item.map(item => 
+	// <li className="singleItem">
+	// 	<img src="https://seeklogo.com/images/A/adidas-logo-9AA835C1C2-seeklogo.com.png" />
+	// 	<p>Adidas</p>
+	// 	<p>8</p>
+	// </li>
+	render() {
+		return (
+			<ul>
+				<li className="singleItem">
+					<img src="https://seeklogo.com/images/A/adidas-logo-9AA835C1C2-seeklogo.com.png" />
+					<p>Adidas</p>
+					<p>8</p>
+				</li>
+			</ul>
+		)
+	}
+}
+	
 export default Sizes;

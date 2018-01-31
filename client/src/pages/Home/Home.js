5e42abde70fc4b356d2d79f7eea135f1a69cbd36
Holly Livingston <hlivingston07@gmail.com>
2018-01-31 12:39:23 -0600

    style changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -54,7 +54,7 @@ class Home extends Component {
         return (
             <div className="appBody">
                 {!this.hasSearched ? (<div className="beforeSearch"><Search handleSearch={this.handleSearch} handleInputChange={this.handleInputChange}/></div>)
-                : (<div><div className="afterSearch"><Search handleSearch={this.handleSearch} handleInputChange={this.handleInputChange}/></div><Sizes /></div>)}
+                : (<div className="afterSearchContainer"><div className="afterSearch"><Search handleSearch={this.handleSearch} handleInputChange={this.handleInputChange}/></div><Sizes /></div>)}
             </div>
             
         )

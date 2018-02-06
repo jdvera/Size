2f1cde10e14c7a14ae257668bd4f2e0f945bca17
dmaddox <dmaddox@gmail.com>
2018-02-05 22:55:53 -0600

    field validation in place

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -16,7 +16,8 @@ class Home extends Component {
         hips: "",
         results: [],
         hasSearched: false,
-        dataOnFile: false
+        dataOnFile: false,
+        errorMessage: ""
     };
 
     // get any saved user data
@@ -54,52 +55,66 @@ class Home extends Component {
         // this.hasSearched = true;
         
         if (this.state.type === "shoes") {
-            if (this.state.brand) {
-                API.getShoes({
-                    brand: this.state.brand,
-                    gender: this.state.gender,
-                    shoe: this.state.shoe
-                }).then(res => {
-                    console.log(res.data);
-                    this.setState({ results: [res.data], hasSearched: true, dataOnFile: false });
-                    console.log(this.state);
-                }).catch(err => console.log(err));
-            }
-            else {
-                console.log("BOOGIE: " + this.state.shoe)
-                API.getShoesWithoutBrand({
-                    gender: this.state.gender,
-                    shoe: this.state.shoe
-                }).then(res => {
-                    console.log(res.data);
-                    this.setState({ results: res.data, hasSearched: true, dataOnFile: false });
-                    console.log(this.state);
-                }).catch(err => console.log(err));
+
+            // field validation
+            if (!this.state.shoe || !this.state.gender) {
+                this.setState({ errorMessage: "Please input gender & foot size."});
+            } else {
+
+                if (this.state.brand) {
+                    API.getShoes({
+                        brand: this.state.brand,
+                        gender: this.state.gender,
+                        shoe: this.state.shoe
+                    }).then(res => {
+                        console.log(res.data);
+                        this.setState({ results: [res.data], hasSearched: true, dataOnFile: false });
+                        console.log(this.state);
+                    }).catch(err => console.log(err));
+                }
+                else {
+                    console.log("BOOGIE: " + this.state.shoe)
+                    API.getShoesWithoutBrand({
+                        gender: this.state.gender,
+                        shoe: this.state.shoe
+                    }).then(res => {
+                        console.log(res.data);
+                        this.setState({ results: res.data, hasSearched: true, dataOnFile: false });
+                        console.log(this.state);
+                    }).catch(err => console.log(err));
+                }
             }
         }
         else if (this.state.type === "dresses") {
-            if (this.state.brand) {
-                API.getDresses({
-                    brand: this.state.brand,
-                    bust: this.state.bust,
-                    waist: this.state.waist,
-                    hips: this.state.hips
-                }).then(res => {
-                    console.log(res.data);
-                    this.setState({ results: [res.data], hasSearched: true, dataOnFile: false });
-                    console.log(this.state);
-                }).catch(err => console.log(err));
-            }
-            else {
-                API.getDressesWithoutBrand({
-                    bust: this.state.bust,
-                    waist: this.state.waist,
-                    hips: this.state.hips
-                }).then(res => {
-                    console.log(res.data);
-                    this.setState({ results: res.data, hasSearched: true, dataOnFile: false });
-                    console.log(this.state);
-                }).catch(err => console.log(err));
+
+            // field validation
+            if (!this.state.bust || !this.state.waist || !this.state.hips) {
+                this.setState({ errorMessage: "Please input all measurements."});
+            } else {
+
+                if (this.state.brand) {
+                    API.getDresses({
+                        brand: this.state.brand,
+                        bust: this.state.bust,
+                        waist: this.state.waist,
+                        hips: this.state.hips
+                    }).then(res => {
+                        console.log(res.data);
+                        this.setState({ results: [res.data], hasSearched: true, dataOnFile: false });
+                        console.log(this.state);
+                    }).catch(err => console.log(err));
+                }
+                else {
+                    API.getDressesWithoutBrand({
+                        bust: this.state.bust,
+                        waist: this.state.waist,
+                        hips: this.state.hips
+                    }).then(res => {
+                        console.log(res.data);
+                        this.setState({ results: res.data, hasSearched: true, dataOnFile: false });
+                        console.log(this.state);
+                    }).catch(err => console.log(err));
+                }
             }
         }
     };
@@ -112,6 +127,7 @@ class Home extends Component {
                 <div className={(noSearch) ? "beforeSearch" : "afterSearch"}>
                     <Search type={this.state.type} handleSearch={this.handleSearch} handleInputChange={this.handleInputChange} />
                 </div> 
+                <div className="h3">{this.state.errorMessage}</div>
                 {(!noSearch ? <Sizes results={this.state.results}/> : "")}
                 {(hasData ? <Sizes results={this.state.results}/> : "")}
             </div>
